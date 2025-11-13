d5ac7f9eb05f09a1635c4db0c6dbf0e8159af715
linuxtusX5 <salvatusjoffrey@gmail.com>
2025-11-13 14:38:12 +0800

    category

---
+++ src/pages/Categories.tsx
@@ -0,0 +1,22 @@
+import { Box } from "@mui/material";
+
+function Categories() {
+  return (
+    <Box sx={{ background: "#F9F5EE" }}>
+      <h1
+        style={{
+          fontSize: "5rem",
+          fontWeight: "bold",
+          marginBottom: "1rem",
+          fontFamily: "Didot",
+          textAlign: "center",
+          color: "#1a1a1a",
+        }}
+      >
+        Shop By Category
+      </h1>
+    </Box>
+  );
+}
+
+export default Categories;
