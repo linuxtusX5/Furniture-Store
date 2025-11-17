63a1b7d48625ff244be2e07d062c872b09c60d66
linuxtusX5 <salvatusjoffrey@gmail.com>
2025-11-17 17:28:29 +0800

    Categories

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,4 +1,12 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  CardMedia,
+} from "@mui/material";
 
 function Categories() {
   return (
@@ -15,6 +23,44 @@ function Categories() {
       >
         Shop By Category
       </h1>
+      <p
+        style={{
+          marginBottom: "1rem",
+          textAlign: "center",
+          color: "#1a1a1a",
+        }}
+      >
+        Explore our carefully curated collections
+      </p>
+      <Box sx={{ display: "flex", justifyContent: "center", gap: "30px" }}>
+        <Card
+          sx={{ minWidth: 350, background: "#F9F5EE", borderStyle: "none" }}
+        >
+          <CardContent sx={{ padding: "0px" }}>
+            <CardMedia
+              sx={{ height: 250 }}
+              image="/src/assets/img/pexels-fotoaibe-1643383.jpg"
+              title="green iguana"
+            />
+            <Box sx={{ margin: "20px" }}>
+              <Typography
+                variant="h5"
+                component="div"
+                sx={{
+                  fontFamily: "Didot",
+                  fontSize: "2rem",
+                  fontWeight: "bold",
+                }}
+              >
+                Living Room
+              </Typography>
+              <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+                Comfort Meets Style
+              </Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
     </Box>
   );
 }
