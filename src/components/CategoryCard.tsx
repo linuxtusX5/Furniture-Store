6b22236b37d0c56fe6cea625090cdf4d6a7195ef
linuxtusX5 <salvatusjoffrey@gmail.com>
2025-11-20 11:46:10 +0800

    Minor Adjustment

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { Box, Card, CardContent, Typography, CardMedia } from "@mui/material";
-import type { Product } from "../types";
+import type { Category } from "../types";
 import { useNavigate } from "react-router-dom";
 
 interface Props {
-  product: Product;
+  category: Category;
 }
-export const CategoryCard: React.FC<Props> = ({ product }) => {
+export const CategoryCard: React.FC<Props> = ({ category }) => {
   const navigate = useNavigate();
 
   return (
     <Box sx={{ background: "#F9F5EE" }}>
       <Box sx={{ display: "flex", justifyContent: "center", gap: "30px" }}>
         <Card
-          onClick={() => navigate(`/products/${product.slug}`)}
+          onClick={() => navigate(`/${category.slug}`)}
           sx={{ minWidth: 350, background: "#F9F5EE", borderStyle: "none" }}
         >
           <CardContent sx={{ padding: "0px" }}>
             <CardMedia
+              component="img"
               sx={{ height: 250 }}
-              image="/src/assets/img/pexels-fotoaibe-1643383.jpg"
-              title="green iguana"
+              image={category.image || ""}
+              alt={category.name}
+              title={category.name}
             />
             <Box sx={{ margin: "20px" }}>
               <Typography
@@ -32,10 +34,10 @@ export const CategoryCard: React.FC<Props> = ({ product }) => {
                   fontWeight: "bold",
                 }}
               >
-                Living Room
+                {category.name}
               </Typography>
               <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
-                Comfort Meets Style
+                {category.description}
               </Typography>
             </Box>
           </CardContent>
